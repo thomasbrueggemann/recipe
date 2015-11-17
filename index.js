700e672cafcf2fbebd5a2620fd3120a675dfb018
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-11-17 19:54:20 +0100

    no message

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 var s = "3/16";
 
 function quantity(s) {
-  var parts = s.split("/");
-  
-  // no slash included, just return number
-  if(parts.length == 1) return parseFloat(s);
-  
-  // parse "1/2" to 0.5
-  return parseFloat(parts[0]) * (1.0 / parseFloat(parts[1]));
+	var parts = s.split("/");
+
+	// no slash included, just return number
+	if (parts.length == 1) return parseFloat(s);
+
+	// parse "1/2" to 0.5
+	return parseFloat(parts[0]) * (1.0 / parseFloat(parts[1]));
 }
 
 var units = ["teaspoon", "tablespoon", "cup"];
-var data = [];
 
 // START CONVERSION
 $("#start").on("click", function() {
-  var input = $("#in").val();
-  
-  // loop all lines of recipe
-  var lines = input.split("\n");
-  for(var i in lines) {
-    
-    // split line into components
-    var line = lines[i].split(" ");
-    
-    var unitpos = -1;
-    
-    // try to find unit in line
-    for(var j in line) {
-      
-      // loop available units
-      for(var u in units) {
-        if(line[j].indexOf(units[u]) >= 0) {
-          unitpos = j;
-          break;
-        }
-      }
-      
-      if(unitpos > -1) break;
-    }
-    
-    // unit found so add to data array
-    if(unitpos > -1) {
-      data.push({
-        "quantity": quantity(line[unitpos - 1]),
-        "unit": line[unitpos],
-        "ingredient": line.slice(unitpos + 1).join(" ")
-      });
-    }
-  }
-  
-  console.table(data);
+	var data = [];
+	var input = $("#in").val();
+
+	// loop all lines of recipe
+	var lines = input.split("\n");
+	for (var i in lines) {
+
+		// split line into components
+		var line = lines[i].split(" ");
+
+		var unitpos = -1;
+
+		// try to find unit in line
+		for (var j in line) {
+
+			// loop available units
+			for (var u in units) {
+				if (line[j].indexOf(units[u]) >= 0) {
+					unitpos = j;
+					break;
+				}
+			}
+
+			if (unitpos > -1) break;
+		}
+
+		// unit found so add to data array
+		if (unitpos > -1) {
+			data.push({
+				"quantity": quantity(line[unitpos - 1]),
+				"unit": line[unitpos],
+				"ingredient": line.slice(parseInt(unitpos) + 1).join(" ")
+			});
+		}
+	}
+
+	console.table(data);
 });
