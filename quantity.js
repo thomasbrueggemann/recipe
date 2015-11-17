b36d0789e32f837f7c54785f0c1bf5a138debb9c
Thomas Brüggemann <thomas@informme.de>
2015-11-17 11:09:51 +0100

    Update quantity.js

--- quantity.js
+++ quantity.js
@@ -1,5 +1,8 @@
 var s = "3/16";
 
-var parts = s.split("/");
-var result = parseFloat(parts[0]) * (1.0 / parseFloat(parts[1]));
-console.log(result);
+function quantity(s) {
+  var parts = s.split("/");
+  return parseFloat(parts[0]) * (1.0 / parseFloat(parts[1]));
+}
+
+console.log(quantity(s));
